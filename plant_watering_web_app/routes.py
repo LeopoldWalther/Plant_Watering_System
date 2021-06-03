1109a9eed09339a53f7a8c293020fe1278fadc33
LeopoldWalther <leopold.walther@gmail.com>
2021-06-03 10:35:48 +0200

    feat: add control center route

--- plant_watering_web_app/routes.py
+++ plant_watering_web_app/routes.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Congratulations, it's a web app!"
+    return "Leo will control his plant watering system from all over the world with this web app!"
+
+
+@app.route('/control-center.html')
+def control_center():
+    message = 'Hello World'
+    return render_template('control-center.html', message=message)
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
