6daee839358211386508b3b32013730d67409491
LeopoldWalther <leopold.walther@gmail.com>
2021-06-04 11:35:35 +0200

    feat: add current time and status text and make buttons for auto watering responsive

--- plant_watering_web_app/routes.py
+++ plant_watering_web_app/routes.py
@@ -1,16 +1,51 @@
 from plant_watering_web_app import app
+import datetime
 import json
 import plotly
 from flask import render_template
 
 
+def message_template(text=""):
+    current_time = datetime.datetime.now().strftime("%d.%m-%Y, %H:%M:%S")
+    message = {
+        'time': current_time,
+        'text': text
+    }
+    return message
+
 @app.route('/')
 @app.route('/index')
 def index():
-    message = 'Leo will control his plant watering system from all over the world with this web app!'
-    return render_template('dashboard.html', message=message)
+    message = message_template()
+    return render_template('dashboard.html', **message)
 
 
 @app.route('/cakes')
 def cakes():
     return 'Yummy cakes!'
+
+
+@app.route("/auto/water/<toggle>")
+def auto_water(toggle):
+    running = False
+    if toggle == "ON":
+        message = message_template(text="Auto Watering On")
+        """
+        for process in psutil.process_iter():
+            try:
+                if process.cmdline()[1] == 'auto_water.py':
+                    message = message_template(text ="Already running")
+                    running = True
+            except:
+                pass
+        if not running:
+            os.system("python3.4 auto_water.py&")
+        """
+    else:
+        message = message_template(text="Auto Watering Off")
+
+        """
+        os.system("pkill -f water.py")
+        """
+
+    return render_template('dashboard.html', **message)
