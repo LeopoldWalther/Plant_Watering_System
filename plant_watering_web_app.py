6b83021cb6b88a94a580cc394e7942b70a7fd35b
LeopoldWalther <leopold.walther@gmail.com>
2021-06-13 14:07:36 +0200

    config: deactivate flask debug mode

--- plant_watering_web_app.py
+++ plant_watering_web_app.py
@@ -2,7 +2,7 @@ from plant_watering_web_app import app
 
 
 def main():
-    app.run(host='0.0.0.0', port=31415, debug=True)
+    app.run(host='0.0.0.0', port=31415, debug=False)
 
 
 if __name__ == '__main__':
