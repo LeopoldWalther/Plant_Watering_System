f851bb77180a2cbbc13376aab575dc9912324ac2
LeopoldWalther <leopold.walther@gmail.com>
2021-06-13 15:00:09 +0200

    fix: chain commands in shell

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-source "/home/pi/Documents/Projects/PlantWateringSystem/Plant_Watering_System/env380/bin/activate" &
-python3 plant_watering_web_app.py &
+source "/home/pi/Documents/Projects/PlantWateringSystem/Plant_Watering_System/env380/bin/activate" && python3 plant_watering_web_app.py &
